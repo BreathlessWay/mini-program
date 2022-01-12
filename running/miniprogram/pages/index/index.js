aa504ba84f985ae05dc3530dbca1ba633d1a5e32
BreathlessWay <731005087@qq.com>
2022-01-12 10:59:21 +0800

    perf: 添加dayjs lodash.get

--- running/miniprogram/pages/index/index.js
+++ running/miniprogram/pages/index/index.js
@@ -1,17 +1,10 @@
-Page({
-  data: {},
-  async onLoad() {
-    // const soulSoupData = await wx.cloud.callFunction({
-    //   name: "running",
-    //   data: {
-    //     type: "getBmiData",
-    //     height: 170,
-    //     weight: 80,
-    //     gender: 1,
-    //   },
-    // });
+const lget = require("../../miniprogram_npm/lodash.get/index");
 
-    // console.log(soulSoupData);
+Page({
+  data: {
+    userInfo: null,
+  },
+  async onLoad() {
     await this.login();
     // await this.getWeather();
   },
@@ -23,7 +16,9 @@ Page({
           type: "login",
         },
       });
-      console.log(userInfo);
+      this.setData({
+        userInfo: lget(userInfo, "result.data"),
+      });
     } catch (error) {
       console.log(error.errMsg);
     }
@@ -45,5 +40,14 @@ Page({
     } catch (error) {
       console.log(error.errMsg);
     }
+  },
+  async getUserInfo(e) {
+    const soulSoupData = await wx.cloud.callFunction({
+      name: "running",
+      data: {
+        type: "login",
+        userInfo: e.detail.userInfo,
+      },
+    });
   },
 });
