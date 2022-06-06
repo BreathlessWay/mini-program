ae7ac66886b431f74c343b85bbb077a8305d6795
BreathlessWay <731005087@qq.com>
2022-06-06 12:46:51 +0800

    no message

--- cloudfunctions/notice/index.js
+++ cloudfunctions/notice/index.js
@@ -5,6 +5,8 @@ const cloud = require('wx-server-sdk'),
 
 cloud.init()
 
+const time = 24 * 60 * 60 * 1000;
+
 // 云函数入口函数
 exports.main = async () => {
   const log = cloud.logger();
@@ -29,13 +31,18 @@ exports.main = async () => {
       } = userInfo;
 
       if (lastHeSuanTime && expiration && shouldNotice) {
-        const lastTime = dayjs(lastHeSuanTime).add(expiration, 'hour').format('YYYY年MM月DD日');
+
+        const lastTime = dayjs(lastHeSuanTime).add(expiration, 'hour'),
+          between = (dayjs().valueOf() - lastTime.valueOf()) < time;
+
+        if (!between) return;
+
         await userMapDb.doc(userInfo._id).update({
           data: {
             shouldNotice: false
           },
         });
-        
+
         await cloud.openapi.subscribeMessage.send({
           "touser": OPENID,
           "page": 'pages/index/index',
@@ -47,7 +54,7 @@ exports.main = async () => {
               "value": '核酸到期提醒小贴士'
             },
             "date2": {
-              "value": lastTime
+              "value": lastTime.format('YYYY年MM月DD日')
             },
             "thing3": {
               "value": '⚠️：核酸还有不到24小时就要到期啦！'
